08cce9931e66feb21db3cea07c5832cca12bbdaa
HanJunGeum <84554709+HanJun-g0id@users.noreply.github.com>
2025-07-02 17:01:21 +0900

    Update Optimization.py

--- src/Optimization/Optimization.py
+++ src/Optimization/Optimization.py
@@ -84,3 +84,8 @@ def test_loop(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
